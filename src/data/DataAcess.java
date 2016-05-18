5a52065a2e15a64dd6dc924dd8e239a1cdfb153e
Elisa <Elisa@MoonWitch>
2016-05-18 15:22:16 +0200

    Connection VIRKER!!!!!

--- src/data/DataAcess.java
+++ src/data/DataAcess.java
@@ -3,6 +3,7 @@ package data;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DataAcess {
@@ -10,19 +11,35 @@ public class DataAcess {
 	
 	ResultSet resultset = null;
 	// Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-	String url = "jdbc:sqlserver://10.153.246.10:1433;instanceName=SQLExpress;databaseName=IPSOS;user=Elisa;password=12345678";
-	
+	//MSSQLSERVER
+	String url = "jdbc:sqlserver://localhost:1433;instanceName=SQLExpress;databaseName=MFTS;integratedSecurity=true;";
+
+	//String url = "jdbc:sqlserver://10.153.246.10:1433;instanceName=SQLExpress;databaseName=IPSOS;user=Elisa;password=12345678";
 	public void getConnection(){
-		Connection con = DriverManager.getConnection(url);
-		Statement statement = con.createStatement();
-		resultset = statement.executeQuery("arg0");
+		Connection con;
+		
+		try {
+			con = DriverManager.getConnection(url);
+			Statement statement = con.createStatement();
+			resultset = statement.executeQuery("SELECT * FROM Bestilling");
+			
+			while(resultset.next()){
+				System.out.println(resultset);
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println(e);
+		}
+		
 	}
-	
+	/*
 	con.commit();
 	con.rollback();
 	con.close();
 	con.connection();
-	
+	*/
 
 	
 	
---
+++ src/data/TestDataAcess.java
@@ -0,0 +1,13 @@
+package data;
+
+public class TestDataAcess {
+
+	public static void main(String[] args) {
+
+		DataAcess data = DataAcess.getInstance();
+		data.getConnection();
+		
+		
+	}
+
+}
