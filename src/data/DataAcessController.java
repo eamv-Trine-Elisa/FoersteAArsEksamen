e4414579967555c37427f7f169b7c86131669535
Elisa <Elisa@MoonWitch.ceuherning.int>
2016-05-20 11:22:41 +0200

    Tilføjet Test og laver Controller mere rigtig

--- src/data/DataAcessController.java
+++ src/data/DataAcessController.java
@@ -8,30 +8,29 @@ import logik.FindOplysningerFraBestilnr;
 public class DataAcessController {
 
 	public static void main(String[] args) {
-	
+
+		findAlleBestillingerOgExporterCSV(1);
+	}
+
+	public static void findAlleBestillingerOgExporterCSV(int kundenr) {
 		ArrayList<String> overføre = new ArrayList<>();
-		
+
 		int bestilID = 0;
-		int kundenr = 1;
-		
+
+
 		FindBestilIDFraKundenr fbIDfk = new FindBestilIDFraKundenr();
 		FindOplysningerFraBestilnr fofb = new FindOplysningerFraBestilnr();
 		CSV csv = CSV.getInstance();
-		
-		
+
 		overføre.addAll(fbIDfk.findeBestilIDfraKundenr(kundenr));
-		
-		
-		
+
 		for (int i = 0; i < overføre.size(); i++) {
-		
-		bestilID = Integer.parseInt(fbIDfk.findeBestilIDfraKundenr(kundenr).get(i));
-		
-		csv.lavCSVfil(fofb.bestillingAdresseOplysninger(bestilID));
+
+			bestilID = Integer.parseInt(fbIDfk.findeBestilIDfraKundenr(kundenr).get(i));
+
+			csv.lavCSVfil(fofb.bestillingAdresseOplysninger(bestilID));
 		}
-		
-		
-		
+
 	}
 
 }
---
+++ src/test/TestAfDataAcess.java
@@ -0,0 +1,10 @@
+package test;
+
+public class TestAfDataAcess {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
