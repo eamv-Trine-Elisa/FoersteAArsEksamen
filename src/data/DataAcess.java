48f2751f149080b4f7f70d0e98d47e10bbd5b0f4
Trine <Trine@Skole-pc>
2016-05-24 13:41:48 +0200

    Vi lavede noget igen :D

--- src/data/DataAcess.java
+++ src/data/DataAcess.java
@@ -13,7 +13,8 @@ public class DataAcess {
 	private ResultSet resultset = null;
 	private Connection con;
 	String url = "jdbc:sqlserver://localhost:1433;instanceName=SQLExpress;databaseName=MFTS;integratedSecurity=true;";
-
+	boolean korrektIndsat = false;
+	
 	// Creating a singleton class.
 	private static DataAcess instance = null;
 
@@ -59,6 +60,82 @@ public class DataAcess {
 
 		return resultset;
 
+	}
+	
+	public boolean indsætIBestilling(int kundenr) {
+		
+		try {
+			con = DriverManager.getConnection(url);
+			Statement statement = con.createStatement();
+			statement.executeUpdate("INSERT INTO Bestilling VALUES("+ kundenr +")");
+			korrektIndsat = true;
+			
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			korrektIndsat = false;
+		}
+		
+		
+		return korrektIndsat;
+		
+	}
+	public boolean indsætIBestilling_adresse(int bestilID, String startVejOgNr, String startBy, String startPostnr,
+			String startKommune, String slutVejOgNr, String slutBy, String slutPostnr, String slutKommune) {
+		try {
+			con = DriverManager.getConnection(url);
+			Statement statement = con.createStatement();
+			statement.executeUpdate("INSERT INTO Bestilling_Adresse VALUES ("+ bestilID +",'"+ startVejOgNr +"',' "+ startBy +"','"+ startPostnr +"','"+ startKommune +"',"
+					+ "' "+ slutVejOgNr +"',' "+ slutBy +"',' "+ slutPostnr +"',' "+ slutKommune +")");
+			
+			korrektIndsat = true;
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			
+			korrektIndsat = false;
+		}
+		
+		return korrektIndsat;	
+	}
+	
+	public boolean indsætIBestilling_PHB(int bestilID, String antalPersoner,String hjælpemidler, String beregnetPris) {
+		try {
+			con = DriverManager.getConnection(url);
+			Statement statement = con.createStatement();
+			statement.executeUpdate("INSERT INTO Bestilling_PHB VALUES ("+ bestilID +",'"+ antalPersoner +"','"+ hjælpemidler +"','"+ beregnetPris +")");
+			
+			korrektIndsat = true;
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			
+			korrektIndsat = false;
+		}
+		
+		return korrektIndsat;
+	}
+	
+	public boolean indsætIBestilling_Tid(int bestilID, String dato,String måned, String afgang_ankomst,String klokkeslæt) {
+		try {
+			con = DriverManager.getConnection(url);
+			Statement statement = con.createStatement();
+			statement.executeUpdate("INSERT INTO Bestilling_Tid VALUES ("+ bestilID +",'"+ dato +"','"+ måned +"','"+ afgang_ankomst +"','"+ klokkeslæt +"')");
+			
+			korrektIndsat = true;
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			korrektIndsat = false;
+		}
+		
+		
+		return korrektIndsat;
+		
 	}
 
 }
