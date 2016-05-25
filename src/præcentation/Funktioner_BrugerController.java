5cfccca2eeb125b971492f2647d9f2ad22b85615
Elisa <Elisa@MoonWitch>
2016-05-25 16:41:47 +0200

    GUI pis

--- src/præcentation/Funktioner_BrugerController.java
+++ src/præcentation/Funktioner_BrugerController.java
@@ -3,9 +3,13 @@ package præcentation;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
 
 public class Funktioner_BrugerController implements Initializable{
 
@@ -13,16 +17,28 @@ public class Funktioner_BrugerController implements Initializable{
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
 	}
-	
+
 	@FXML
-	private void haandterBestilKnap(){
-		System.out.println("Trykket");
+	private void haandterBestilKnap(ActionEvent event) {
+		
 	}
 
 	@FXML
-	private void haandterPerson(){
+	private void haandterPerson() {
 		System.out.println("1");
 	}
+
+	@FXML
+	private void startKommune() {
+//		ChoiceBox = countChoiceBox;
+//		countChoiceBox.setItems(FXCollections.observableArrayList("One","Two","Three"));
+		//FXCollections.observableArrayList("First", "Second", "Third");
+	}
 	
-	
+	@FXML
+	private void dag(){
+		TextField dag = null;
+		dag.getText();
+	}
+
 }
