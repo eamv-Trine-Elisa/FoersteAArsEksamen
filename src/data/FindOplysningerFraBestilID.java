e2cf941580283f95f81aa7d682f6a0c31506657a
Trine <Trine@Skole-pc>
2016-06-01 07:18:20 +0200

    ændringer i data

--- src/data/FindOplysningerFraBestilID.java
+++ src/data/FindOplysningerFraBestilID.java
@@ -38,4 +38,58 @@ public class FindOplysningerFraBestilID {
 		System.out.println(resultater);
 		return resultater;
 	}
+	public ArrayList<String> bestillingTidOplysninger(int bestilID) {
+
+		try {
+			DataAcess da = DataAcess.getInstance();
+			resultset = da.bestillingsOplysninger("Bestilling_Tid", bestilID);
+
+			while (resultset.next()) {
+				ResultSetMetaData metadata = resultset.getMetaData();
+				int numberOfColumns = metadata.getColumnCount();
+
+				int i = 1;
+				while (i <= numberOfColumns) {
+
+					resultater.add(resultset.getString(i));
+					
+				i++;
+				}
+			}
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println(resultater);
+		return resultater;
+	}
+	public ArrayList<String> bestillingPHBOplysninger(int bestilID) {
+
+		try {
+			DataAcess da = DataAcess.getInstance();
+			resultset = da.bestillingsOplysninger("Bestilling_PHB", bestilID);
+
+			while (resultset.next()) {
+				ResultSetMetaData metadata = resultset.getMetaData();
+				int numberOfColumns = metadata.getColumnCount();
+
+				int i = 1;
+				while (i <= numberOfColumns) {
+
+					resultater.add(resultset.getString(i));
+					
+				i++;
+				}
+			}
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println(resultater);
+		return resultater;
+	}
 }
--- src/data/HåndteringAFSamleTilCSV.java
+++ src/data/HåndteringAFSamleTilCSV.java
@@ -6,21 +6,30 @@ public class HåndteringAFSamleTilCSV {
 	
 	public void findAlleBestillingerOgExporterCSV(int kundenr){
 		ArrayList<String> overføre = new ArrayList<>();
+		ArrayList<String> samledeOplysninger = new ArrayList<>();
 
-		int bestilID = 0;
+		int bestilID = 1;
 
 		FindBestilIDFraKundenr fbIDfk = new FindBestilIDFraKundenr();
 		FindOplysningerFraBestilID fofb = new FindOplysningerFraBestilID();
 		CSV csv = CSV.getInstance();
 
 		overføre.addAll(fbIDfk.findeBestilIDfraKundenr(kundenr));
-
-		for (int i = 0; i < overføre.size(); i++) {
-
-			bestilID = Integer.parseInt(fbIDfk.findeBestilIDfraKundenr(kundenr).get(i));
-
-			csv.lavCSVfil(fofb.bestillingAdresseOplysninger(bestilID));
+		
+		samledeOplysninger.addAll(fofb.bestillingPHBOplysninger(bestilID));
+		samledeOplysninger.addAll(fofb.bestillingTidOplysninger(bestilID));
+		samledeOplysninger.addAll(fofb.bestillingAdresseOplysninger(bestilID));
+		
+		for (int i = 0; i < samledeOplysninger.size(); i++) {
+			System.out.println(samledeOplysninger.get(i));
 		}
+
+//		for (int i = 0; i < overføre.size(); i++) {
+//
+//			bestilID = Integer.parseInt(fbIDfk.findeBestilIDfraKundenr(kundenr).get(i));
+//
+//			csv.lavCSVfil(fofb.bestillingAdresseOplysninger(bestilID));
+//		}
 
 	}
 }
