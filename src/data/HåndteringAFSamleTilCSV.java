810ddb1d02416b8730949465c28e1a2aee5cd8c9
Trine <Trine@Skole-pc>
2016-06-01 08:37:13 +0200

    data ændret igen

--- src/data/HåndteringAFSamleTilCSV.java
+++ src/data/HåndteringAFSamleTilCSV.java
@@ -8,7 +8,7 @@ public class HåndteringAFSamleTilCSV {
 		ArrayList<String> overføre = new ArrayList<>();
 		ArrayList<String> samledeOplysninger = new ArrayList<>();
 
-		int bestilID = 1;
+		int bestilID = 0;
 
 		FindBestilIDFraKundenr fbIDfk = new FindBestilIDFraKundenr();
 		FindOplysningerFraBestilID fofb = new FindOplysningerFraBestilID();
@@ -16,20 +16,20 @@ public class HåndteringAFSamleTilCSV {
 
 		overføre.addAll(fbIDfk.findeBestilIDfraKundenr(kundenr));
 		
-		samledeOplysninger.addAll(fofb.bestillingPHBOplysninger(bestilID));
-		samledeOplysninger.addAll(fofb.bestillingTidOplysninger(bestilID));
-		samledeOplysninger.addAll(fofb.bestillingAdresseOplysninger(bestilID));
 		
-		for (int i = 0; i < samledeOplysninger.size(); i++) {
-			System.out.println(samledeOplysninger.get(i));
-		}
+		
+		
 
-//		for (int i = 0; i < overføre.size(); i++) {
-//
-//			bestilID = Integer.parseInt(fbIDfk.findeBestilIDfraKundenr(kundenr).get(i));
-//
-//			csv.lavCSVfil(fofb.bestillingAdresseOplysninger(bestilID));
-//		}
+		for (int i = 0; i < overføre.size(); i++) {
+
+			bestilID = Integer.parseInt(fbIDfk.findeBestilIDfraKundenr(kundenr).get(i));
+			samledeOplysninger.addAll(fofb.bestillingPHBOplysninger(bestilID));
+			samledeOplysninger.addAll(fofb.bestillingTidOplysninger(bestilID));
+			samledeOplysninger.addAll(fofb.bestillingAdresseOplysninger(bestilID));
+
+			
+		}
+		csv.lavCSVfil(samledeOplysninger);
 
 	}
 }
